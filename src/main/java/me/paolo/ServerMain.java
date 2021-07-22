c7325ec21df388da0e067f80c7aac8f72528ecf0
CrackheadV <pviola153@gmail.com>
2021-07-22 20:15:32 +0200

    Changed format and added Updater

--- src/main/java/me/paolo/ServerMain.java
+++ src/main/java/me/paolo/ServerMain.java
@@ -1,14 +1,13 @@
 package me.paolo;
 
+import me.paolo.client.ArtifactStruct;
 import me.paolo.client.ClientInfo;
 import me.paolo.client.JarInfo;
 import me.paolo.manager.StaticClient;
 import me.paolo.util.IOUtil;
 import me.paolo.util.JsonUtil;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -21,8 +20,19 @@ public class ServerMain
     private ServerMain() { }
 
     public static void main(String[] args) {
-        { // Import all clients
-            File versionsDir = new File("./versions");
+        if(args.length < 2) {
+            System.out.println("Usage: <versions_dir> <update_file>");
+            return;
+        }
+
+        { // Import all clients (First part)
+            File versionsDir = new File(args[0]);
+            if(!versionsDir.exists()) {
+                System.err.println("Versions dir not found!");
+                return;
+            }
+
+            System.out.println("Importing clients...");
             ClientInfo[] clientList = StaticClient.list(versionsDir);
             Arrays.sort(clientList, Comparator.comparing(ClientInfo::getName));
 
@@ -38,8 +48,62 @@ public class ServerMain
             }
             catch (IOException e) {
                 e.printStackTrace();
+                return;
             }
         }
+        System.out.println();
+        { // Import update file (Second part)
+            File updateFile = new File(new File(args[1]).getAbsolutePath());
+            if(!updateFile.exists()) {
+                System.err.println("Update file not found!");
+                return;
+            }
+
+            File updateParent = updateFile.getParentFile();
+            if(updateParent == null) {
+                System.err.println("Parent cannot be null!");
+                return;
+            }
+
+            System.out.println("Importing update file...");
+            ArtifactStruct artifact = getArtifact(updateFile, updateFile);
+            try(FileOutputStream outputStream = new FileOutputStream(
+                    IOUtil.createFile(new File(updateParent, "Penumbra Launcher.json"), false)
+            )) {
+                String json = JsonUtil.toJsonStr(artifact);
+                outputStream.write(json.getBytes(StandardCharsets.UTF_8));
+                outputStream.flush();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static ArtifactStruct getArtifact(File rootDir, File file) {
+        String rootAbsolute = rootDir.getAbsolutePath().replace('\\', '/');
+        String fileAbsolute = file.getAbsolutePath().replace('\\', '/');
+
+        String parentAbsolute = new File(rootAbsolute).getParentFile()
+                .getAbsolutePath().replace('\\', '/');
+
+        ArtifactStruct artifact = new ArtifactStruct();
+        artifact.setPath((rootAbsolute.equals(fileAbsolute) ?
+                fileAbsolute.substring(parentAbsolute.length() + 1) : fileAbsolute.substring(rootAbsolute.length() + 1)));
+        artifact.setUrl(REPOSITORY_URL + '/' + fileAbsolute.substring(parentAbsolute.length() + 1));
+        artifact.setSize(file.length());
+
+        try(FileInputStream inputStream = new FileInputStream(file);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            IOUtil.storeSha1Checksum(inputStream, outputStream);
+            artifact.setSha1(IOUtil.toHexString(outputStream.toByteArray()));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return artifact;
     }
 
 }
--- src/main/java/me/paolo/manager/StaticClient.java
+++ src/main/java/me/paolo/manager/StaticClient.java
@@ -1,13 +1,10 @@
 package me.paolo.manager;
 
 import me.paolo.ServerMain;
-import me.paolo.client.ArtifactStruct;
 import me.paolo.client.ClientInfo;
 import me.paolo.client.JarInfo;
-import me.paolo.util.IOUtil;
 import me.paolo.util.JsonUtil;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -21,51 +18,38 @@ public class StaticClient
 
     private StaticClient() { }
 
-    private static ArtifactStruct createArtifact(ClientInfo clientInfo, String version, File file) {
-        ArtifactStruct artifact = new ArtifactStruct();
-        artifact.setPath(clientInfo.getName() + '-' + version + '/' + file.getName());
-        artifact.setSize(file.length());
-        artifact.setUrl(String.format("%s/versions/%s/%s/%s",
-                ServerMain.REPOSITORY_URL, clientInfo.getName(), version, file.getName()
-        ));
-
-        try(FileInputStream inputStream = new FileInputStream(file);
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-            IOUtil.storeSha1Checksum(inputStream, outputStream);
-            artifact.setSha1(IOUtil.toHexString(outputStream.toByteArray()));
-        }
-        catch (IOException e) {
-            e.printStackTrace();
+    private static ClientInfo get(File clientDir) {
+        File rootDir = clientDir.getParentFile();
+        if(rootDir == null) {
+            System.err.println("Parent directory is null. This shouldn't happen!");
             return null;
         }
 
-        return artifact;
-    }
-
-    private static ClientInfo get(File clientDir) {
         try(FileInputStream inputStream = new FileInputStream(new File(clientDir, "config.json"))) {
             ClientInfo clientInfo = JsonUtil.fromJson(inputStream, ClientInfo.class);
 
             String[] previews = clientInfo.getPreviewUrls();
             for(int i = 0; i < previews.length; i++) {
-                previews[i] = String.format("%s/versions/%s/%s",
-                        ServerMain.REPOSITORY_URL, clientDir.getName(), previews[i]);
+                previews[i] = Objects.requireNonNull(
+                        ServerMain.getArtifact(rootDir, new File(clientDir, previews[i]))
+                ).getUrl();
             }
 
             List<JarInfo> jars = new ArrayList<>();
             Arrays.stream(Objects.requireNonNull(clientDir.listFiles()))
                     .filter(File::isDirectory)
-                    .forEach(file -> {
+                    .forEach(verionDir -> {
                         JarInfo jarInfo = new JarInfo();
-                        jarInfo.setJar(createArtifact(clientInfo, file.getName(), new File(
-                                file, clientInfo.getName() + ".jar"
+                        jarInfo.setJar(ServerMain.getArtifact(rootDir, new File(
+                                verionDir, clientInfo.getName() + ".jar"
                         )));
-
-                        jarInfo.setJson(createArtifact(clientInfo, file.getName(), new File(
-                                file, clientInfo.getName() + ".json"
+                        jarInfo.setJson(ServerMain.getArtifact(rootDir, new File(
+                                verionDir, clientInfo.getName() + ".json"
                         )));
-                        jarInfo.setVersion(file.getName());
+                        jarInfo.setVersion(verionDir.getName());
                         jars.add(jarInfo);
+
+                        System.out.println("Imported: \"" + clientInfo.getName() + '-' + jarInfo.getVersion() + "\"");
                     });
             clientInfo.setJars(jars.toArray(new JarInfo[0]));
             return clientInfo;
